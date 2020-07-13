045504211e42bdd3919962350251cda4cdf4f2e4
Danny Ramos <danramos21@gmail.com>
2020-07-13 17:07:08 -0600

    merchants passing

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -58,30 +58,3 @@ describe "Merchants API" do
   end
 end
 
-# describe 'Merchants' do
-#       xit 'can get a merchant' do
-#         response = conn('/api/v1/merchants/42').get
-
-#         expected_attributes = {
-#           name: 'Glover Inc',
-#         }
-
-#         json = JSON.parse(response.body, symbolize_names: true)
-
-#         expect(json[:data][:id]).to eq('42')
-
-#         expected_attributes.each do |attribute, value|
-#           expect(json[:data][:attributes][attribute]).to eq(value)
-#         end
-#       end
-
-#       xit 'can get all merchants' do
-#         response = conn('/api/v1/merchants').get
-#         json = JSON.parse(response.body, symbolize_names: true)
-
-#         expect(json[:data].length).to eq(100)
-#         json[:data].each do |merchant|
-#           expect(merchant[:type]).to eq("merchant")
-#           expect(merchant[:attributes]).to have_key(:name)
-#         end
-#       end
