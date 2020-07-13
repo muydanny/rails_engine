f8c55628cd91efb1ade363f445c278390c36f764
Danny Ramos <danramos21@gmail.com>
2020-07-13 15:56:17 -0600

    index spec for merchant

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
       resources :items, only: [:index, :show, :create, :destroy, :update]
     end
   end
+
+  namespace :api do 
+    namespace :v1 do 
+      resources :merchants, only: [:index, :show, :create, :destroy, :update]
+    end
+  end
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -5,17 +5,66 @@ describe "Merchants API" do
 
     merchant = create(:merchant)
 
-    get "/api/v1/items/#{merchant.id}"
+    get "/api/v1/merchants/#{merchant.id}"
 
     expect(response).to be_successful
     
     single_item = JSON.parse(response.body)
     expect(response).to be_successful
-    expect(item['name']).to eq(item.name)
-    expect(item['description']).to eq(item.description)
-    expect(item['unit_price']).to eq(item.unit_price)
-    expect(item['merchant_id']).to eq(item.merchant_id)
+    expect(merchant['name']).to eq(merchant.name)
   end
+
+  it "can get all merchants" do 
+    merchant = create(:merchant)
+    merchant = create(:merchant)
+    
+    get "/api/v1/merchants"
+    expect(response).to be_successful
+    body = JSON.parse(response.body)
+    expect(body["data"].count).to eq(2)
+  end
+
+  # it "can create an item" do 
+  #   merchant = create(:merchant)
+    
+  #   item = build(:item, merchant: merchant)
+
+  #   post '/api/v1/items', params: item.attributes
+
+  #   expect(response).to be_successful
+  #   body = response.body 
+  #   response = JSON.parse(body)
+    
+  #   expect(response["data"]["attributes"]["name"]).to eq(item.name)
+  #   expect(response["data"]["attributes"]["description"]).to eq(item.description)
+  # end
+
+  # it "can delete an item" do 
+  #   merchant = create(:merchant)
+  #   item = create(:item)
+  #   expect(Item.count).to eq(1)
+
+  #   expect{ delete "/api/v1/items/#{item.id}" }.to change(Item, :count).by(-1)
+
+  #   expect(response).to be_successful
+  #   expect(Item.count).to eq(0)
+  #   expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  # end
+
+  # it "can update an item"  do 
+  #   merchant = create(:merchant)
+  #   item = create(:item)
+  #   updated_item = build(:item, merchant: merchant)
+
+  #   patch "/api/v1/items/#{item.id}", params: updated_item.attributes
+    
+  #   expect(response).to be_successful
+    
+  #   expect(item.name).to eq(updated_item.name)
+  #   expect(item.description).to eq(updated_item.description)
+  #   expect(item.unit_price).to eq(updated_item.unit_price)
+  #   expect(merchant.id).to eq(updated_item.merchant_id)
+  # end
 end
 
 # describe 'Merchants' do
