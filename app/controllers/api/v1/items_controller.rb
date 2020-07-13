b61caa652af4c79fe1dc32571ccd3fece6d93aea
Danny Ramos <danramos21@gmail.com>
2020-07-12 22:52:53 -0600

    item create wip

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -9,14 +9,16 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new(item_params)
-    if item.save 
-      render json: ItemSerializer.new(item)  
-    end
+    render json: ItemSerializer.new(Item.create(item_params))
+  end
+
+  def update
+    render json: ItemSerializer.new(Item.update(params[:id], item_params))
   end
 
   def destroy
-    render json: Item.delete(params[:id])
+    render json: ItemSerializer.new(Item.find(params[:id]))
+    Item.delete(params[:id])
   end
 
   private 
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -54,13 +54,31 @@ describe "Items API" do
     expect(Item.count).to eq(0)
     expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
   end
+
+  it "can update an item"  do 
+    merchant = create(:merchant)
+    item = create(:item)
+    previous_item = Item.last 
+    item_params = {name: "Shiny Itemy Item", description: "I want to play a game", unit_price: 5011.96, merchant_id: merchant.id}
+
+    put "/api/v1/items/#{item.id}", params: {item: item_params}
+
+    updated_item = Item.find_by(id: item.id)
+    
+    expect(response).to be_successful
+    expect(updated_item.name).to_not eq(previous_item)
+    expect(updated_item.name).to eq(item_params[:name])
+    expect(updated_item.description).to eq(item_params[:description])
+    expect(updated_item.unit_price).to eq(item_params[:unit_price])
+    expect(updated_item.merchant_id).to eq(item_params[:merchant_id])
+  end
 end
 
 
-# it 'can create and delete an item' do
+# it 'can update an item' do
 #         name = "Shiny Itemy Item"
 #         description = "It does a lot of things real good"
-#         unit_price = 5011.96
+#         unit_price = 5011
 #         merchant_id = 43
 
 #         body = {
@@ -70,27 +88,24 @@ end
 #           merchant_id: merchant_id
 #         }
 
-#         # Create a item
-#         response = conn('/api/v1/items').post do |request|
+#         response = conn('/api/v1/items/75').patch do |request|
 #           request.body = body
 #         end
 
 #         json = JSON.parse(response.body, symbolize_names: true)
+#         item = json[:data]
+#         expect(item[:attributes][:name]).to eq(name)
+#         expect(item[:attributes][:description]).to eq(description)
+#         expect(item[:attributes][:unit_price]).to eq(unit_price)
+#         expect(item[:attributes][:merchant_id]).to eq(merchant_id)
 
-#         new_item = json[:data]
-#         expect(new_item[:attributes][:name]).to eq(name)
-#         expect(new_item[:attributes][:description]).to eq(description)
-#         expect(new_item[:attributes][:unit_price]).to eq(unit_price)
-#         expect(new_item[:attributes][:merchant_id]).to eq(merchant_id)
-
-#         # Delete a item
-#         delete_response = conn("/api/v1/items/#{new_item[:id]}").delete
-
-#         json = JSON.parse(delete_response.body, symbolize_names: true)
-
-#         deleted_item = json[:data]
-#         expect(deleted_item[:attributes][:name]).to eq(name)
-#         expect(deleted_item[:attributes][:description]).to eq(description)
-#         expect(deleted_item[:attributes][:unit_price]).to eq(unit_price)
-#         expect(deleted_item[:attributes][:merchant_id]).to eq(merchant_id)
+#         original_body = {
+#           name: 'Item Autem Eligendi',
+#           description:'Aliquam dolores dolore voluptas nesciunt non praesentium. Eum nihil repellendus modi. Aut in expedita nesciunt. Ut aliquam dicta omnis voluptas.',
+#           unit_price: '29949',
+#           merchant_id: '3',
+#         }
+#         conn("/api/v1/items/").patch do |request|
+#           request.body = original_body
+#         end
 #       end
